f679c7653775ba4a2d2dd68b1698b8ed23b05604
janmarsino98 <wiskyscapit@gmail.com>
2024-05-08 18:06:04 +0200

    Hemos hecho una comparacion de python y js

--- frontend/src/components/SelectSection.jsx
+++ frontend/src/components/SelectSection.jsx
@@ -38,6 +38,7 @@ const SelectSection = () => {
           if (forYouSelected) {
             return;
           } else {
+            l;
             setForYouSelected(false);
           }
         }}
---
+++ prueba.py
@@ -0,0 +1,7 @@
+def sum(x,y):
+    return x + y
+
+
+function sum(x, y) {
+    return x + y
+}
